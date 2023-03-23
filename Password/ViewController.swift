bb706e8c9ff8f10fbc88ebf426f1b6869b98479f
VuslatYolcu <ylcvalt@gmail.com>
2023-03-23 22:10:27 +0300

    Shifting elements

--- Password/ViewController.swift
+++ Password/ViewController.swift
@@ -165,16 +165,18 @@ extension ViewController {
         
         // check if the top of the keyboard is above the bottom of the currently focused textbox
         let keyboardTopY = keyboardFrame.cgRectValue.origin.y
+        
         let convertedTextFieldFrame = view.convert(currentTextField.frame, from: currentTextField.superview)
+
         let textFieldBottomY = convertedTextFieldFrame.origin.y + convertedTextFieldFrame.size.height
 
         // if textField bottom is below keyboard bottom - bump the frame up
         if textFieldBottomY > keyboardTopY {
-            // adjust view up
+            let textBoxY = convertedTextFieldFrame.origin.y
+            let newFrameY = (textBoxY - keyboardTopY / 2) * -1
+            view.frame.origin.y = newFrameY
         }
 
-        print("foo - currentTextFieldFrame: \(currentTextField.frame)")
-        print("foo - convertedTextFieldFrame: \(convertedTextFieldFrame)")
     }
 
     @objc func keyboardWillHide(notification: NSNotification) {
