1f3c86cd07d08c9c0520eff601c0609d99488dd2
VuslatYolcu <ylcvalt@gmail.com>
2023-03-26 12:41:57 +0300

    Add unit tests for password status view validate method

--- PasswordTests/PasswordStatusViewTests.swift
+++ PasswordTests/PasswordStatusViewTests.swift
@@ -74,3 +74,46 @@ class PasswordStatusViewTests_ShowCheckmarkOrRedX_When_Validation_Is_Loss_Of_Foc
         XCTAssertTrue(statusView.lengthCriteriaView.isXmarkImage) // ❌
     }
 }
+
+class PasswordStatusViewTests_Validate_Three_Of_Four_Conditions: XCTestCase {
+    
+    var statusView: PasswordStatusView!
+    
+    let validPassword = "12345678Aa!"
+    
+    let shortPassword = "A"
+    let spacedPassword = "12122 343"
+    let noUppercasePassword = "12345678aa!"
+    let noLowercasePassword = "12345678AA!"
+    let noSpecialCharacterAndNoUppercasePassword = "12345678aaa"
+    
+    override func setUp() {
+        super.setUp()
+        statusView = PasswordStatusView()
+    }
+    
+    func testValidPassword() throws {
+        XCTAssertTrue(statusView.validate(validPassword))
+    }
+
+    func testTooShort() throws {
+        XCTAssertFalse(statusView.validate(shortPassword))
+    }
+    
+    func testNoSpaceConditionNotMet() throws {
+        XCTAssertFalse(statusView.validate(spacedPassword))
+    }
+    
+    func testOnlyUppercaseNotMet() throws {
+        XCTAssertTrue(statusView.validate(noUppercasePassword))
+    }
+    
+    func testOnlyLowercasecaseNotMet() throws {
+        XCTAssertTrue(statusView.validate(noLowercasePassword))
+    }
+    
+    func testNoSpecialCharacterAndNoUppercaseNotMet() throws {
+        XCTAssertFalse(statusView.validate(noSpecialCharacterAndNoUppercasePassword))
+    }
+    
+}
